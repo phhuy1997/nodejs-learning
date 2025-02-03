cd5053480b4c72c4f2737d27a0878ae555a242f1
Phuoc Huy Nguyen <phuochuy_nguyen@datahouse.com>
2025-02-03 16:23:47 +0700

    Express: API POST, GET, Request.Body, Request.Params

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -9,9 +9,9 @@ const getABC = (req, res) => {
 	// TO DO: procress data, call Model, ...
 	let users = [];
 	connection.query(
-		'SELECT * FROM Users',
+		'SELECT * FROM Users WHERE `name` = ?', [req.params.abc],
 		function (err, results, fields) {
-			results.forEach((result) => users.push(result.name));
+			results.forEach((result) => users.push(`${result.name} (${result.email})`));
 			console.log('users :>> ', users);
 			res.send(`Hello, ${users.join(" and ")}!`); // --> return response to client
   }
@@ -29,10 +29,27 @@ const getViewRender = (req, res) => {
 	res.render('./sample.ejs'); //reponse the html file sample.ejs to client
 }
 
+const postCreateUser = (req, res) => {
+	// TO DO: procress data, call Model, ...
+	console.log("Request Body: ", req.body);
+	// let email = req.body.email; // let name = req.body.name; // let city = req.body.city;
+	let {email, name, city} = req.body;
+	connection.query(
+		`INSERT INTO Users (email, name, city) VALUES (?,?,?)`, [email, name, city],
+		function (err, results) {
+			console.log(results); // 
+			res.send('Created User Successfully');
+		}
+
+	)
+
+}
+
 
 module.exports = {
 	getHomePage,
 	getABC,
 	getHtmlRender,
 	getViewRender,
+	postCreateUser,
 }
--- src/routes/web.js
+++ src/routes/web.js
@@ -1,6 +1,6 @@
 // This file is used for server-site rendering
 const express = require('express')
-const { getHomePage, getABC, getHtmlRender, getViewRender } = require('../controllers/homeController')
+const { getHomePage, getABC, getHtmlRender, getViewRender, postCreateUser } = require('../controllers/homeController')
 const router = express.Router()
 
 // middleware that is specific to this router
@@ -15,11 +15,13 @@ router.use(timeLog)
 // Declare Our Routes:
 router.get('/', getHomePage); // Respective to this route, call to Controller: getHomePage
 
-router.get('/abc', getABC); // Respective to this route, call to Controller: getABC
+router.get('/:abc', getABC); // Respective to this route, call to Controller: getABC
 
 router.get('/htmlRender', getHtmlRender); // Respective to this route, call to Controller: getHtmlRender
 
 router.get('/viewRender', getViewRender); // Respective to this route, call to Controller: getViewRender
+
+router.post('/create-user', postCreateUser);
 
 
 module.exports = router
