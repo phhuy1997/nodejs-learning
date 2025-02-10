371f96c8bc83e7e9e8a03bb403daa304c58bdf9d
Phuoc Huy Nguyen <phuochuy_nguyen@datahouse.com>
2025-02-10 09:37:49 +0700

    Express: Middleware setup

--- src/server.js
+++ src/server.js
@@ -14,13 +14,32 @@ const hostname=process.env.HOST_NAME; // read HOST_NAME from .env
 
 
 
+// Declare this middleware to convert request json data to Object --> help BE easily read request.body
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+// Declare this middleware to check logic (like Authenticate) --> If pass, can go to the main Route below
+app.use((req, res, next) => {
+  if (req.method === 'GET') {
+    console.log('This is GET Method');
+    next(); // --> accept to continue to below
+  } else {
+    console.log('This is POST Method');
+    next(); // --> accept to continue to below
+  }
+})
+
+
 
 // declare router for Express App
 app.use('/', webRoutes); // '/' is prefix of the route
 app.use('/api/v1/', apiRoutes); // '/' is prefix of the API endpoint
 
 
-
+// Config this middleware to redirect user to notFoundPage.ejs if all routes above was not meet
+app.use((req, res) => {
+  return res.render('./notFoundPage.ejs')
+})
 
 
 // config template (view) engine, static files --> see in moved to /config/viewEngine.js
